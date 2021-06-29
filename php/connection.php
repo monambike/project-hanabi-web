06e84c4adc9263e63d700019e12a431f4c41f9d2
monambike <vinicius_gabriel258@hotmail.com>
2021-06-29 17:06:58 -0300

    Update 'connection.php' documentation

--- php/connection.php
+++ php/connection.php
@@ -1,14 +1,33 @@
 <?php
-	// Rede Local
+	// FAZ A CONEXÃO COM O BANCO
+	// -------------------------------------------------------------
+	// Descrição:
+	// Realiza a conexão com o banco.
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	
+
+	
+	// CONEXÕES
+	// -------------------------------------------------------------
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	// Estabelece a conexão com a Rede Local
 	$con = mysqli_connect('Localhost','root','','projecthanabidb');
 	
-	// WebHost
+	// Estabelece a conexão em WebHost
 	//$con = mysqli_connect('Localhost','id13846883_monambike','05X[VOY<ahR&!iYk','id13846883_projecthanabidb');
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	// -------------------------------------------------------------
 
+	// Executa a conexão com o banco
 	if(mysqli_connect_error()){
 		echo
 		'<script>
 			window.open("../../errors/database_connection.html", "_self");
 		</script>';
 	}
+	
+
+
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+	// -------------------------------------------------------------
 ?>
